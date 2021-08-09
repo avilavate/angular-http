d42ae162d761cfa17a1dac41fb273acf5b53eeb7
A <avilavate@live.in>
2021-08-09 21:01:15 +0530

    A sample project explorin angular HttpClient (http) service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  NgForm
+} from '@angular/forms';
+import {
+  HttpClient
+} from '@angular/common/http';
+import {
+  Post
+} from './posts';
+import {map} from "rxjs/operators";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-http-demo';
+  loadedData: Post[] = [];
+  error:Error|null = null;
+
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.fetchPosts();
+  }
+  submit(f: NgForm) {
+    this.http.post(
+        'https://recipe-app-service-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json',
+        f.value)
+      .subscribe(respData => {
+        this.fetchPosts();
+        this.error=null;
+      },error=>{
+        this.error=new Error(error.message);
+      });
+
+      f.resetForm();
+     
+  }
+
+  fetchPosts() {
+    this.http.get < {[key: string]: Post} > (
+        'https://recipe-app-service-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json',
+      )
+      .pipe(
+        map(
+        data => {
+        let out: Post[] = [];
+        let keys = Object.keys(data);
+        for(const key in data) {
+          out.push({...data[key]});
+        }
+        return out;
+      })
+      )
+      .subscribe(respData => {
+        this.loadedData=respData;
+        console.log(respData);
+        this.error=null;
+      },error=>{
+        this.error=new Error(error.message);
+      });
+  }
+
+  clearAll() {
+    this.http.delete('https://recipe-app-service-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json').
+    subscribe(() => {
+      this.loadedData = [];
+      this.error=null;
+    }, error=>{
+      this.error=new Error(error.message);
+    })
+
+  }
+
 }
